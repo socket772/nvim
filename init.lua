e7ec2882649d4c4240ddb31a15b3793a603f11e5
socket772 <socket772@protonmail.com>
2024-08-28 23:57:00 +0200

    Aggiunto formatter on save

--- init.lua
+++ init.lua
@@ -6,16 +6,35 @@ require("config.lazy")
 require("catppuccin")
 vim.cmd.colorscheme "catppuccin-mocha"
 
-require("mason").setup({ PATH = "append"})
+require("mason").setup({ PATH = "append" })
 require("mason-lspconfig").setup()
 
 require("mason-lspconfig").setup_handlers {
 	-- The first entry (without a key) will be the default handler
 	-- and will be called for each installed server that doesn't have
 	-- a dedicated handler.
-	function (server_name) -- default handler (optional)
+	function(server_name) -- default handler (optional)
 		require("lspconfig")[server_name].setup {}
 	end,
 }
 
 require('lualine').setup()
+
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		-- Conform will run multiple formatters sequentially
+		python = { "isort", "black" },
+		-- You can customize some of the format options for the filetype (:help conform.format)
+		rust = { "rustfmt", lsp_format = "fallback" },
+		-- Conform will run the first available formatter
+		javascript = { "prettierd", "prettier", stop_after_first = true },
+	},
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
+})
